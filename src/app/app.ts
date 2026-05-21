bf7031c65ede3f0623c28f67ebfa4a130474b8ad
thienlv <parnerthienlv@abbank.vn>
2026-05-21 16:29:24 +0700

    remove code unused

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NativeBridgeService } from './native-bridge.service';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +10,9 @@ import { NativeBridgeService } from './native-bridge.service';
 export class App implements OnInit {
   protected readonly title = signal('native-web');
   messageFromNative = signal<string>('No messages yet');
-  
-  private nativeBridge = inject(NativeBridgeService);
+
 
   ngOnInit() {
-    this.nativeBridge.messages$.subscribe(msg => {
-      // Pretty print JSON response from native
-      this.messageFromNative.set(JSON.stringify(msg, null, 2));
-    });
-  }
-
-  sendHelloToNative() {
-    this.nativeBridge.sendMessageToNative('GREETING', { text: 'Hello from Web JS (Angular)!' });
-  }
-
-  requestDataFromNative() {
-    this.nativeBridge.sendMessageToNative('REQUEST_DATA', { type: 'USER_INFO' });
+ 
   }
 }
